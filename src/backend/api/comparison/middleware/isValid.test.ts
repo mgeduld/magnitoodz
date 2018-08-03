53f466c2871e78b6c515bdc64231145b490aa452
Marcus Geduld <mgeduld@gmail.com>
2018-08-03 18:30:41 -0400

    fixed math error

--- src/backend/api/comparison/middleware/isValid.test.ts
+++ src/backend/api/comparison/middleware/isValid.test.ts
@@ -2,31 +2,36 @@ import test from 'ava'
 import { isValidId } from './isValidId'
 
 test('comparison::middleware::isValidId makes sure param.id is a number', (t: any) => {
-    let actual1
-    let actual2
-    let actual3
+  let actual1
+  let actual2
+  let actual3
 
-    isValidId(
-        { params: { id: 25 } },
-        { json: () => Promise.resolve() },
-        (value) => { actual1 = value }
-    )
-    t.is(actual1, undefined)
+  isValidId(
+    { params: { id: 25 } },
+    { json: () => Promise.resolve() },
+    (value) => {
+      actual1 = value
+    }
+  )
+  t.is(actual1, undefined)
 
-    isValidId(
-        { params: { id: '2' } },
-        { json: () => Promise.resolve() },
-        (value) => { actual2 = value }
-    )
+  isValidId(
+    { params: { id: '2' } },
+    { json: () => Promise.resolve() },
+    (value) => {
+      actual2 = value
+    }
+  )
 
-    t.is(actual1, undefined)
+  t.is(actual1, undefined)
 
-    isValidId(
-        { params: { id: 'cat' } },
-        { json: () => Promise.resolve() },
-        (value) => { actual3 = value }
-    )
+  isValidId(
+    { params: { id: 'cat' } },
+    { json: () => Promise.resolve() },
+    (value) => {
+      actual3 = value
+    }
+  )
 
-    t.deepEqual(actual3, new Error('Invalid id cat'))
+  t.deepEqual(actual3, new Error('Invalid id cat'))
 })
-
--- src/frontend/modules/magnitoodz/components/magnitood.tsx
+++ src/frontend/modules/magnitoodz/components/magnitood.tsx
@@ -6,79 +6,80 @@ import { Comparison } from './comparison'
 import { FinalChunk } from '../enums/chunk'
 import { colors } from '../colors'
 
-
 const renderSmallMagnitude = ({
-    bigMagnitude,
+  bigMagnitude,
+  smallMagnitude,
+  smallMagnitudeName,
+  maxChunks,
+  unit,
+  spans = [],
+  key = 0
+}) => {
+  const chunkSize = bigMagnitude / maxChunks
+  const currentColor = colors[spans.length]
+  const nextColor = colors[spans.length + 1]
+  const smallPercentOfBig = smallMagnitude / bigMagnitude
+  const numChunks = smallPercentOfBig * maxChunks
+  const hasFractionalPart = numChunks % 1 !== 0
+  const wholeNumChunks = hasFractionalPart ? Math.ceil(numChunks) : numChunks
+  if (smallMagnitude >= chunkSize) {
+    return [
+      ...spans,
+      <div key={key}>
+        <p className="b i">
+          {smallMagnitudeName && <span>{smallMagnitudeName} </span>}
+          <span>{smallMagnitude} </span>
+          {unit && <span>{unit}</span>}
+        </p>
+        <MagnitoodSpan
+          chunks={wholeNumChunks}
+          last={hasFractionalPart ? FinalChunk.dim : FinalChunk.normal}
+          color={currentColor}
+        />
+        <p>
+          <span> One dash (</span>
+          <span className={currentColor}>-</span>
+          <span>
+            ) = {Math.round(smallMagnitude / wholeNumChunks)} {unit ? unit : ''}
+          </span>
+        </p>
+      </div>
+    ]
+  }
+  const soFar = [
+    ...spans,
+    <Comparison
+      key={key}
+      bigChunkSize={chunkSize}
+      smallChunkSize={wholeNumChunks}
+      unit={unit}
+      chunks={maxChunks}
+      comparisonColor="white"
+      bigMagnituteColor={currentColor}
+      smallMagnitudeColor={nextColor}
+    />
+  ]
+  return renderSmallMagnitude({
     smallMagnitude,
     smallMagnitudeName,
     maxChunks,
     unit,
-    spans = [],
-    key = 0
-}) => {
-    const chunkSize = bigMagnitude / maxChunks
-    const currentColor = colors[spans.length]
-    const nextColor = colors[spans.length + 1]
-    const smallPercentOfBig = smallMagnitude / bigMagnitude
-    const numChunks = smallPercentOfBig * maxChunks
-    const hasFractionalPart = numChunks % 1 != 0
-    const wholeNumChunks = hasFractionalPart ? Math.ceil(numChunks) : numChunks
-    if (smallMagnitude >= chunkSize) {
-        return [
-            ...spans,
-            <div key={key}>
-                <p className="b i">
-                    {smallMagnitudeName && <span>{smallMagnitudeName} </span>}
-                    <span>{smallMagnitude} </span>
-                    {unit && <span>{unit}</span>}
-                </p>
-                <MagnitoodSpan
-                    chunks={wholeNumChunks}
-                    last={hasFractionalPart ? FinalChunk.dim : FinalChunk.normal}
-                    color={currentColor}
-                />
-            </div>
-        ]
-    } else {
-        const soFar = [
-            ...spans,
-            <Comparison
-                key={key}
-                bigChunkSize={chunkSize}
-                smallChunkSize={wholeNumChunks}
-                unit={unit}
-                chunks={maxChunks}
-                comparisonColor="white"
-                bigMagnituteColor={currentColor}
-                smallMagnitudeColor={nextColor}
-            />
-        ]
-        return renderSmallMagnitude({
-            bigMagnitude: chunkSize,
-            smallMagnitude,
-            smallMagnitudeName,
-            maxChunks,
-            unit,
-            spans: soFar,
-            key: key + 1
-        })
-    }
-}
-
-const getStartingSmallComparisonChunkSize = ({ smallMagnitude, bigMagnitude, bigMagnitudeChunkSize, maxChunks }) => {
-    return smallMagnitude <= bigMagnitudeChunkSize ? (bigMagnitude / maxChunks) : bigMagnitude
+    bigMagnitude: chunkSize,
+    spans: soFar,
+    key: key + 1
+  })
 }
 
 interface IProps {
-    id: number,
-    magnitood: IComparison,
-    onInit: Function
+  id: number
+  magnitood: IComparison
+  onInit: Function
 }
 
 /*
 Unfortunately for those of us who prefer functional components,
 this one needs to be a class, because it has to access its props
-in componentDidMount. 
+in componentDidMount.
 
 We can use recompose to add a componentDidMount method to a functional
 component, but it won't have acces to props. :-(
@@ -86,66 +87,61 @@ component, but it won't have acces to props. :-(
 TODO: figure out if there's a better way!
 */
 class Magnitood extends React.Component<IProps> {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    componentDidMount() {
-        this.props.onInit(this.props.id)
-    }
+  componentDidMount() {
+    this.props.onInit(this.props.id)
+  }
 
-    render() {
-        if (this.props.magnitood.span_1_magnitude) {
-            const {
-                title,
-                span_1_name,
-                span_2_name,
-                span_1_magnitude,
-                span_2_magnitude,
-                unit,
-                description,
-                user_name
-            } = this.props.magnitood
-            const maxChunks = 100
-            const bigMagnitude = Math.max(span_1_magnitude, span_2_magnitude)
-            const smallMagnitude = Math.min(span_1_magnitude, span_2_magnitude)
-            const smallMagnitudeName = smallMagnitude === span_1_magnitude ? span_1_name : span_2_name
-            const bigMagnitudeChunkSize = bigMagnitude / maxChunks
-            const startingSmallComparisonChunkSize = getStartingSmallComparisonChunkSize({ smallMagnitude, bigMagnitude, bigMagnitudeChunkSize, maxChunks })
-            return (
-                <div className='w-100'>
-                    {title && <h2>{title}</h2>}
-                    <p>author: {user_name}</p>
-                    {description && <p>{description}</p>}
-                    <div className="ml3 mt4">
-                        <p className="b i">
-                            {span_1_name && <span >{span_1_name} </span>}
-                            <span>{span_1_magnitude} </span>
-                            {unit && <span>{unit}</span>}
-                        </p>
-                        <MagnitoodSpan
-                            chunks={maxChunks}
-                            color={colors[0]}
-                            highlightColor={colors[1]}
-                        />
-                        {renderSmallMagnitude({
-                            bigMagnitude: startingSmallComparisonChunkSize,
-                            smallMagnitude,
-                            smallMagnitudeName,
-                            maxChunks,
-                            unit
-                        })}
-                    </div>
-                </div>
-            )
-
-        } else {
-            return <div>loading...</div>
-        }
+  render() {
+    if (this.props.magnitood.span_1_magnitude) {
+      const {
+        title,
+        span_1_name,
+        span_2_name,
+        span_1_magnitude,
+        span_2_magnitude,
+        unit,
+        description,
+        user_name
+      } = this.props.magnitood
+      const maxChunks = 100
+      const bigMagnitude = Math.max(span_1_magnitude, span_2_magnitude)
+      const smallMagnitude = Math.min(span_1_magnitude, span_2_magnitude)
+      const smallMagnitudeName =
+        smallMagnitude === span_1_magnitude ? span_1_name : span_2_name
+      const bigMagnitudeChunkSize = bigMagnitude / maxChunks
+      return (
+        <div className="w-100">
+          {title && <h2>{title}</h2>}
+          <p>author: {user_name}</p>
+          {description && <p>{description}</p>}
+          <div className="ml3 mt4">
+            <p className="b i">
+              {span_1_name && <span>{span_1_name} </span>}
+              <span>{span_1_magnitude} </span>
+              {unit && <span>{unit}</span>}
+            </p>
+            <MagnitoodSpan
+              chunks={maxChunks}
+              color={colors[0]}
+              highlightColor={colors[1]}
+            />
+            {renderSmallMagnitude({
+              smallMagnitude,
+              smallMagnitudeName,
+              maxChunks,
+              unit,
+              bigMagnitude
+            })}
+          </div>
+        </div>
+      )
     }
+    return <div>loading...</div>
+  }
 }
 
 export const MagnitoodWithRouter = withRouter(Magnitood)
-
-
-
